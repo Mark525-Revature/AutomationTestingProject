42813bb3a4220b9fb8a494ab6aaadf1136a6fb35
Mark Snyder <Snyder4000@yahoo.com>
2024-08-07 17:24:01 -0400

    added webelements for planet removal

--- automationproject/src/test/java/com/revature/step/PlanetRemovalSteps.java
+++ automationproject/src/test/java/com/revature/step/PlanetRemovalSteps.java
@@ -14,31 +14,31 @@ public class PlanetRemovalSteps {
 
     @When("I change the selector to Planet")
     public void i_change_the_selector_to_Planet(){
-
+        TestRunner.planetariumHome.switchDropdownToPlanet();
     }
 
     @When("I insert the planet name {string}")
     public void i_insert_the_planet_name(String name){
-        
+        TestRunner.planetariumHome.enterCelestialBodyName(name);
     }
 
     @When("I insert no planet name")
     public void i_insert_no_planet_name(){
-
+        TestRunner.planetariumHome.enterCelestialBodyName("");
     }
 
     @When("I click the Delete button")
     public void i_click_the_delete_button(){
-        
+        TestRunner.planetariumHome.deleteCelestialBody();
     }
 
     @Then("the planet should be removed from the planetarium")
     public void the_planet_should_be_removed_from_the_planetarium(){
-
+        Assert.assertEquals("", TestRunner.planetariumHome.getAlertText());
     }
 
     @Then("the planet should not be removed from the planetarium")
     public void the_planet_should_not_be_removed_from_the_planetarium(){
-        
+        Assert.assertEquals("Failed to delete planet with name", TestRunner.planetariumHome.getAlertText());
     }
 }
