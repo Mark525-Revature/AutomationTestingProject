26053b7df4cfdc0845177374ed03915d5f2870bb
Kenneth-299 <kenneth087@revature.net>
2024-08-09 15:40:57 -0500

    View change

--- automationproject/src/test/java/com/revature/step/ViewPlanetAndMoon.java
+++ automationproject/src/test/java/com/revature/step/ViewPlanetAndMoon.java
@@ -12,12 +12,12 @@ public class ViewPlanetAndMoon {
         TestRunner.planetariumLogin.goToLandingPage();
     }
 
-    @When("I provide the username <valid username>")
+    @When("I provide the username {string}")
     public void i_pick_the_option_to_login_View(String username) {
         TestRunner.planetariumLogin.enterUsername(username);
     }
 
-    @When("I provide the password <valid password>")
+    @When("I provide the password {string}")
     public void i_pick_the_option_to_login_Views(String password) {
         TestRunner.planetariumLogin.enterPassword(password);
     }
@@ -27,12 +27,4 @@ public class ViewPlanetAndMoon {
         TestRunner.planetariumLogin.clickLoginButton();
         Assert.assertEquals("Home", TestRunner.driver.getTitle());
     }
-
-    @Then("I should be not sent to the page with planets and moons")
-    public void the_user_should_not_be_logged_in() {
-        TestRunner.planetariumLogin.clickLoginButton();
-        Assert.assertEquals("login attempt failed: please try again", TestRunner.planetariumLogin.getAlertText());
-    }
-
-
 }
