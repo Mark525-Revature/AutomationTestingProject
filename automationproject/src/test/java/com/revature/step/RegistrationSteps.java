46666f4a5157d9b8ea99c9b748b9661e02b98fef
regan587 <regan587@revature.net>
2024-08-14 20:19:07 -0500

    fixed issues with features and steps, removed redundancies, improved formatting

--- automationproject/src/test/java/com/revature/step/RegistrationSteps.java
+++ automationproject/src/test/java/com/revature/step/RegistrationSteps.java
@@ -2,9 +2,9 @@ package com.revature.step;
 
 import org.junit.Assert;
 import com.revature.TestRunner;
-import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import io.cucumber.java.en.Given;
 
 public class RegistrationSteps {
     
