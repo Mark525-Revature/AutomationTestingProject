d05bdde9b4c6da56d0dc1a30ce782bd316420911
Mark Snyder <Snyder4000@yahoo.com>
2024-08-19 12:24:46 -0400

    setup for creating unit tests

---
+++ automationproject/src/test/java/com/revature/controller/ViewControllerTest.java
@@ -0,0 +1,44 @@
+package com.revature.controller;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import com.revature.planetarium.controller.ViewController;
+import com.revature.Setup;
+import io.javalin.Javalin;
+
+public class ViewControllerTest {
+    ViewController viewController;
+    HttpClient webClient;
+    ObjectMapper objectMapper;
+    Javalin app;
+    @Before
+    public void setUp() throws InterruptedException {
+        Setup.resetTestDatabase();
+        viewController = new ViewController();
+        webClient = HttpClient.newHttpClient();
+        objectMapper = new ObjectMapper();
+        app.start(8080);
+        Thread.sleep(1000);
+    }
+    @After
+    public void tearDown(){
+        app.stop();
+    }
+    /* Tests to make:
+     * method: successfull   / unsucessfull
+     * login: ctx.status(200);/ctx.status(400);
+     * home: ctx.status(200);/ctx.status(400);
+     * register: ctx.status(200);/ctx.status(400);
+     * background: ?;/ctx.status(404);
+    */
+}
---
+++ automationproject/src/test/java/com/revature/service/PlanetServiceTest.java
@@ -0,0 +1,34 @@
+package com.revature.service;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.revature.Setup;
+import io.javalin.Javalin;
+
+public class PlanetServiceTest {
+    
+    HttpClient webClient;
+    ObjectMapper objectMapper;
+    Javalin app;
+    @Before
+    public void setUp() throws InterruptedException {
+        Setup.resetTestDatabase();
+        webClient = HttpClient.newHttpClient();
+        objectMapper = new ObjectMapper();
+        app.start(8080);
+        Thread.sleep(1000);
+    }
+    @After
+    public void tearDown(){
+        app.stop();
+    }
+}
